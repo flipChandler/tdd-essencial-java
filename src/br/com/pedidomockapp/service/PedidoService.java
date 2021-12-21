a372acd96bf0242f1fd7d8706fa221080d4cdc1d
flipChandler <felipe.santos11@hotmail.com>
2021-12-21 16:40:54 -0300

    red green salvarPedidoNoBancoDeDados

--- src/br/com/pedidomockapp/service/PedidoService.java
+++ src/br/com/pedidomockapp/service/PedidoService.java
@@ -1,11 +1,14 @@
 package br.com.pedidomockapp.service;
 
 import br.com.pedidomockapp.model.Pedido;
+import br.com.pedidomockapp.repository.PedidoRepository;
 
 public class PedidoService {
 
 	public double lancar(Pedido pedido) {
-		return pedido.getValor() * 0.1;
+		double imposto = pedido.getValor() * 0.1;
+		new PedidoRepository().guardar(pedido);
+		return imposto;
 	}
 
 }
--- test/br/com/pedidomockapp/PedidoServiceTest.java
+++ test/br/com/pedidomockapp/PedidoServiceTest.java
@@ -1,7 +1,8 @@
 package br.com.pedidomockapp;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.pedidomockapp.builder.PedidoBuilder;
@@ -10,16 +11,28 @@ import br.com.pedidomockapp.service.PedidoService;
 
 public class PedidoServiceTest {
 	
+	private Pedido pedido;
+	private PedidoService pedidoService;
+	
+	@BeforeEach
+	private void setup() {
+		pedido = new PedidoBuilder()
+				.comValor(100.0)
+				.para("João", "joao@gmail.com", "99999-0000")
+				.build();
+
+		pedidoService = new PedidoService();
+	}
+	
 	@Test
 	void calcularImposto() throws Exception {
-		Pedido pedido = new PedidoBuilder()
-							.comValor(100.0)
-							.para("João", "joao@gmail.com", "99999-0000")
-							.build();
-		
-		PedidoService pedidoService = new PedidoService();
 		double imposto = pedidoService.lancar(pedido);
 		assertEquals(10.0, imposto, 0.0001);
+	}
+	
+	@Test
+	void salvarPedidoNoBancoDeDados() throws Exception {
+		pedidoService.lancar(pedido);	
 	}
 
 }
