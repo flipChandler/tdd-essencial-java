dcd6377b1fa7d3ca591a6cce738681c9a35f4f8e
flipChandler <felipe.santos11@hotmail.com>
2021-12-21 11:54:26 -0300

    feat refactoring test calcular

--- src/br/com/flyapp/service/PrecoPassagemService.java
+++ src/br/com/flyapp/service/PrecoPassagemService.java
@@ -1,12 +1,17 @@
 package br.com.flyapp.service;
 
 import br.com.flyapp.model.Passageiro;
+import br.com.flyapp.model.TipoPassageiro;
 import br.com.flyapp.model.Voo;
 
 public class PrecoPassagemService {
 
 	public double calcular(Passageiro passageiro, Voo voo) {
-		return 90.0;
+		if (passageiro.getTipo().equals(TipoPassageiro.GOLD)) {
+			return voo.getPreco() * 0.9;
+		}
+		
+		return 0.0;
 	}
 
 }
