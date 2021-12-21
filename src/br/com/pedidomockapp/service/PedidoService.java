d0f2a75213e6931825326213814e81f001633aa7
flipChandler <felipe.santos11@hotmail.com>
2021-12-21 16:51:38 -0300

    mock inicial dos testes

--- src/br/com/pedidomockapp/service/PedidoService.java
+++ src/br/com/pedidomockapp/service/PedidoService.java
@@ -4,10 +4,20 @@ import br.com.pedidomockapp.model.Pedido;
 import br.com.pedidomockapp.repository.PedidoRepository;
 
 public class PedidoService {
+	
+	private PedidoRepository pedidoRepository;
+	
+	
+	
+	public PedidoService(PedidoRepository pedidoRepository) {
+		this.pedidoRepository = pedidoRepository;
+	}
+
+
 
 	public double lancar(Pedido pedido) {
 		double imposto = pedido.getValor() * 0.1;
-		new PedidoRepository().guardar(pedido);
+		this.pedidoRepository.guardar(pedido);
 		return imposto;
 	}
 
--- test/br/com/pedidomockapp/PedidoServiceTest.java
+++ test/br/com/pedidomockapp/PedidoServiceTest.java
@@ -1,37 +1,44 @@
 package br.com.pedidomockapp;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import br.com.pedidomockapp.builder.PedidoBuilder;
 import br.com.pedidomockapp.model.Pedido;
+import br.com.pedidomockapp.repository.PedidoRepository;
 import br.com.pedidomockapp.service.PedidoService;
 
 public class PedidoServiceTest {
 	
-	private Pedido pedido;
 	private PedidoService pedidoService;
 	
 	@BeforeEach
 	private void setup() {
-		pedido = new PedidoBuilder()
-				.comValor(100.0)
-				.para("João", "joao@gmail.com", "99999-0000")
-				.build();
+		PedidoRepository pedidoRepository = Mockito.mock(PedidoRepository.class);
 
-		pedidoService = new PedidoService();
+		pedidoService = new PedidoService(pedidoRepository);
 	}
 	
 	@Test
 	void calcularImposto() throws Exception {
-		double imposto = pedidoService.lancar(pedido);
+		Pedido pedido = new PedidoBuilder()
+							.comValor(100.0)
+							.para("João", "joao@gmail.com", "99999-0000")
+							.build();
+		
+		double imposto = pedidoService.lancar(pedido);		
 		assertEquals(10.0, imposto, 0.0001);
 	}
 	
 	@Test
 	void salvarPedidoNoBancoDeDados() throws Exception {
+		Pedido pedido = new PedidoBuilder()
+				.comValor(100.0)
+				.para("João", "joao@gmail.com", "99999-0000")
+				.build();
 		pedidoService.lancar(pedido);	
 	}
 
